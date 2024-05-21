bfd869217317e6100a9fc4e7aa35828c0dcabd3c
sohyun <kshyun0215@gmail.com>
2024-05-21 16:27:52 +0900

    fix: 테스트 잘못 작성한 부분 수정

--- solution.test.js
+++ solution.test.js
@@ -2,19 +2,19 @@ import { getNextTerm } from './solution';
 
 describe('look and say sequence test', () => {
   describe('첫 번째 항이 1일 때', () => {
-    const expected = [1, 11, 21, 1211, 111221, 312211, 13112221, 113213211];
+    const expected = [1, 11, 21, 1211, 111221, 312211, 13112221, 1113213211];
 
     for (let i = 1; i < expected.length; i++) {
       test(`이전 항이 ${expected[i - 1]}일 때, 현재 항은 ${
         expected[i]
       }이다.`, () => {
-        expect(getNextTerm(expected[i - 1]).toBe(expected[i]));
+        expect(getNextTerm(expected[i - 1])).toBe(expected[i]);
       });
     }
   });
 
   describe('첫 번째 항이 a일 때(1 < a < 10)', () => {
-    let a = Math.floor(Math.random() * 10);
+    let a = Math.floor(Math.random() * 8 + 2);
 
     const prefix = [
       '',
@@ -31,10 +31,8 @@ describe('look and say sequence test', () => {
       test(`이전 항이 ${prefix[i - 1]}a일 때, 현재 항은 ${
         prefix[i]
       }a이다.`, () => {
-        expect(
-          getNextTerm(Number(prefix[i - 1] + a)).toBe(
-            Number(prefix[i - 1] + a),
-          ),
+        expect(getNextTerm(Number(prefix[i - 1] + a))).toBe(
+          Number(prefix[i] + a),
         );
       });
     }
