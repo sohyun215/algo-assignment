fcdded261436241be8894f2423123365bec4532e
sohyun <kshyun0215@gmail.com>
2024-05-21 17:01:02 +0900

    feat: 다음 항 도출하는 함수 작성

--- solution.js
+++ solution.js
@@ -1,1 +1,24 @@
-export function getNextTerm(number) {}
+export function getNextTerm(number) {
+  const curTerm = String(number);
+  let nextTerm = '';
+
+  let repeatedNum = curTerm[0];
+  let cnt = 1; // 반복되는 숫자 개수
+
+  for (let i = 1; i < curTerm.length; i++) {
+    if (repeatedNum === curTerm[i]) {
+      cnt += 1;
+    } else {
+      nextTerm += cnt + repeatedNum; // 반복되는 개수와 숫자를 붙여쓴다.
+
+      repeatedNum = curTerm[i];
+      cnt = 1;
+    }
+  }
+
+  nextTerm += cnt + repeatedNum; // 마지막 숫자에 대한 처리
+
+  return Number(nextTerm);
+}
+
+console.log(getNextTerm(1));
