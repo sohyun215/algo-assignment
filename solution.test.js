f531195c06c826c288d9f6d6295086ee3065ab63
sohyun <kshyun0215@gmail.com>
2024-05-21 15:36:08 +0900

    test: 테스트 코드 반복문으로 수정, 다음 항 도출 함수 이름 수정

--- solution.test.js
+++ solution.test.js
@@ -1,47 +1,42 @@
+import { getNextTerm } from './solution';
+
 describe('look and say sequence test', () => {
   describe('첫 번째 항이 1일 때', () => {
-    test('두 번째 항은 11이다.', () => {
-      expect(solution(1)).toBe(11);
-    });
+    const expected = [1, 11, 21, 1211, 111221, 312211, 13112221, 113213211];
 
-    test('세 번째 항은 21이다.', () => {
-      expect(solution(11).toBe(21));
-    });
-
-    test('네 번째 항은 1211이다.', () => {
-      expect(solution(21).toBe(1211));
-    });
-
-    test('다섯 번째 항은 111221이다.', () => {
-      expect(solution(1211).toBe(111221));
-    });
-
-    test('여섯 번째 항은 312211이다.', () => {
-      expect(solution(111221).toBe(312211));
-    });
+    for (let i = 1; i < expected.length; i++) {
+      test(`이전 항이 ${expected[i - 1]}일 때, 현재 항은 ${
+        expected[i]
+      }이다.`, () => {
+        expect(getNextTerm(expected[i - 1]).toBe(expected[i]));
+      });
+    }
   });
 
   describe('첫 번째 항이 a일 때(1 < a < 10)', () => {
     let a = Math.floor(Math.random() * 10);
 
-    test('두 번째 항은 1a이다.', () => {
-      expect(solution(a).toBe(Number('1' + a)));
-    });
+    const prefix = [
+      '',
+      '1',
+      '111',
+      '311',
+      '13211',
+      '111312211',
+      '31131122211',
+      '1321132132211',
+    ];
 
-    test('세 번째 항은 111a이다.', () => {
-      expect(solution(Number('1' + a)).toBe(Number('111' + a)));
-    });
-
-    test('네 번째 항은 311a이다.', () => {
-      expect(solution(Number('111' + a)).toBe(Number('311' + a)));
-    });
-
-    test('다섯 번째 항은 13211a이다.', () => {
-      expect(solution(Number('311' + a)).toBe(Number('13211' + a)));
-    });
-
-    test('여섯 번째 항은 111312211a이다.', () => {
-      expect(solution(Number('13211' + a)).toBe(Number('11312211' + a)));
-    });
+    for (let i = 1; i < prefix.length; i++) {
+      test(`이전 항이 ${prefix[i - 1]}a일 때, 현재 항은 ${
+        prefix[i]
+      }a이다.`, () => {
+        expect(
+          getNextTerm(Number(prefix[i - 1] + a)).toBe(
+            Number(prefix[i - 1] + a),
+          ),
+        );
+      });
+    }
   });
 });
